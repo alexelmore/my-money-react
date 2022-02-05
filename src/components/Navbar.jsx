6f31948654e95b45f74851455394c409bdfb69c7
alexelmore <alexelmore49@gmail.com>
2022-02-05 10:50:30 -0600

    conditionally show links based on user's logged in status

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,24 +7,28 @@ import styles from "./Navbar.module.css";
 export default function Navbar() {
 	const { logout } = useLogout();
 	const { user } = useAuthContext();
-	console.log("this is user:", user);
+
 	return (
 		<nav className={styles.navbar}>
 			<ul>
 				<li className={styles.title}>myMoney</li>
 
-				<li>
-					<Link to="/login">Login</Link>
-				</li>
-				<li>
-					<Link to="/signup">Sign Up</Link>
-				</li>
-
-				<li>
-					<button onClick={logout} className="btn">
-						Logout
-					</button>
-				</li>
+				{user === null ? (
+					<>
+						<li>
+							<Link to="/login">Login</Link>
+						</li>
+						<li>
+							<Link to="/signup">Sign Up</Link>
+						</li>
+					</>
+				) : (
+					<li>
+						<button onClick={logout} className="btn">
+							Logout
+						</button>
+					</li>
+				)}
 			</ul>
 		</nav>
 	);
