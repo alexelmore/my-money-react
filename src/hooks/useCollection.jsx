60cfa4d6e96bc42ce869bad6959076150ee30ef5
alexelmore <alexelmore49@gmail.com>
2022-02-12 07:47:13 -0600

    finish useCollection hook

--- src/hooks/useCollection.jsx
+++ src/hooks/useCollection.jsx
@@ -8,11 +8,25 @@ export const useCollection = (collection) => {
 	// useEffect hook that fires whenever the collection changes
 	useEffect(() => {
 		let ref = projectFirestore.collection(collection);
-		const unsubscribe = ref.onSnapshot((snapshot) => {
-			let results = [];
-			snapshot.docs.forEach((doc) => {
-				results.push({ ...doc.data(), id: doc.id });
-			});
-		});
+		const unsubscribe = ref.onSnapshot(
+			(snapshot) => {
+				let results = [];
+				snapshot.docs.forEach((doc) => {
+					results.push({ ...doc.data(), id: doc.id });
+				});
+				// Update our documents state with the results variable
+				setDocuments(results);
+				// Update our error state to null
+				setError(error);
+			},
+			(error) => {
+				console.log(error);
+				setError(error.message);
+			}
+		);
+		// Clean up function for the unsubscribe event
+		return () => unsubscribe();
 	}, [collection]);
+	// Return documents and error from our hook
+	return { documents, error };
 };
